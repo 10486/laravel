84a4b2264ebd050bbb80e09b1762cf9935576c5a
10486 <tsyduev.b.1@gmail.com>
2019-11-30 21:44:46 +0500

    hotfix2

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class APIController extends Controller
 {
   public function GetAPI(){
-    $cache = new Memcache;
+    $cache = new Memcached;
     $memcache->connect('memcached', 11212) or die ("Could not connect");
     echo file_get_contents("https://chelhack.deletestaging.com/goods");
     sleep(600);
