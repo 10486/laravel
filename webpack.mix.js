c5fd5c7b99262006a6df7dd3fb49d90f089d34eb
10486 <tsyduev.b.1@gmail.com>
2019-12-01 04:03:37 +0500

    asd

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,12 +11,15 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .js('resources/js/script.js', 'public/js')
-   .js('resources/js/custom.js', 'public/js')
-   .sass('resources/sass/_variables.scss', 'public/css')
-   .styles(['resources/sass/style.css','resources/sass/icomoon.css'], 'public/css')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/js/app.js')
+   .js('resources/js/script.js', 'public/js/app.js')
+   .js('resources/js/custom.js', 'public/js/app.js')
+   .sass('resources/sass/_variables.scss', 'public/css/app.css')
+   .styles(['resources/sass/style.css','resources/sass/icomoon.css'], 'public/css/app.css')
+   .sass('resources/sass/app.scss', 'public/css/app.css')
+   .sass('resources/sass/style.sass', 'public/css/app.css')
+   .sass('resources/sass/style.scss', 'public/css/app.css')
+   .sass('resources/sass/styles.scss', 'public/css/app.css');
 
 
   // mix.js('resources/js/app.js', 'public/js')
