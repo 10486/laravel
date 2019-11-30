a5beb2fe697b94e72e9b4d3a14f8e955614594e9
10486 <tsyduev.b.1@gmail.com>
2019-11-30 21:21:12 +0500

    sdf

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -9,12 +9,8 @@ class APIController extends Controller
   public function GetAPI(){
     $cache = new Memcache;
     $memcache->connect('memcached', 11212) or die ("Could not connect");
-    while (true) {
-      echo file_get_contents("http://chelhack.deletestaging.com/goods");
-      sleep(600)
-      // code...
-    }
-
+    echo file_get_contents("https://chelhack.deletestaging.com/goods");
+    sleep(600)
     return "";
   }
 }
