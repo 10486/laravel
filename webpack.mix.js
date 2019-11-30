9b15b7e39fe828c4f883eeadfc59d34d112df3d0
10486 <tsyduev.b.1@gmail.com>
2019-12-01 04:53:45 +0500

    asd

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,7 +16,7 @@ mix.js('resources/js/script.js', 'public/js/app.js')
    .sass('resources/sass/_variables.scss', 'public/css/app.css')
    .styles(['resources/sass/style.css','resources/sass/icomoon.css'], 'public/css/app.css')
    .sass('resources/sass/style.scss', 'public/css/app.css')
-   .sass('resources/sass/styles.scss', 'public/css/app.css')
+   .sass('resources/sass/catalog.scss', 'public/css/app.css')
    .sass('resources/sass/style.sass', 'public/css/app.css');
 
 
