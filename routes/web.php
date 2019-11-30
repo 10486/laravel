183747e8d278799766db129b304f748c2efc564d
10486 <tsyduev.b.1@gmail.com>
2019-11-30 14:42:48 +0300

    asd

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::get('/', function () {
     return view("welcome");
 });
 Route::get('/black_fiday',"APIController@GetAPI");
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
