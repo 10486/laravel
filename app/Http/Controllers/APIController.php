961f14e13404573f356fa947010feb35a23e5ea2
10486 <tsyduev.b.1@gmail.com>
2019-11-30 21:20:21 +0500

    memcached

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -7,7 +7,13 @@ use Illuminate\Http\Request;
 class APIController extends Controller
 {
   public function GetAPI(){
-    echo file_get_contents("http://chelhack.deletestaging.com/goods");
+    $cache = new Memcache;
+    $memcache->connect('memcached', 11212) or die ("Could not connect");
+    while (true) {
+      echo file_get_contents("http://chelhack.deletestaging.com/goods");
+      sleep(600)
+      // code...
+    }
 
     return "";
   }
