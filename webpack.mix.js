e30046fd0fa28f090b90cb9518336ce7216025fd
10486 <tsyduev.b.1@gmail.com>
2019-12-01 03:48:33 +0500

    d

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,13 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .js('resources/js/script.js', 'public/js')
+   .js('resources/js/custom.js', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css')
+   .sass('resources/sass/_variables.scss', 'public/css')
+   .sass('resources/sass/icomoon.css', 'public/css')
+   .styles(['resources/sass/style.css'], 'public/css')
+   .sass('resources/sass/style.sass', 'public/css')
+   .sass('resources/sass/style.scss', 'public/css')
+   .sass('resources/sass/styles.scss', 'public/css')
