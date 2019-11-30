d2aa69438308607c68a21b8eea721fe5a6550c2b
10486 <tsyduev.b.1@gmail.com>
2019-12-01 03:28:16 +0500

    df

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,16 @@
 <!DOCTYPE html>
 <html lang="ru">
-
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="format-detection" content="telephone=no">
-    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1">
+    <meta name="format-detection" content="telephone=no"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1"/>
     <title>Black Friday</title>
-    <link href="https://fonts.googleapis.com/css?family=Nunito&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito&display=swap" rel="stylesheet"/>
     <link rel="stylesheet" href="/css/app.css"/>
-    <link rel="stylesheet" href="https://unpkg.com/swiper/css/swiper.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper/css/swiper.min.css"/>
 </head>
 <body>
-@include('slider')
-{{"something"}}
+  @include('head')
   <script src="/js/app.js"></script>
 </body>
 </html>
