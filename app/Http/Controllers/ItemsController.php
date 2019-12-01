962958f3623eb57a8e47686e2133b0f25bd9e2d5
10486 <tsyduev.b.1@gmail.com>
2019-12-01 15:12:55 +0500

    sdfsd

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -6,18 +6,5 @@ use Illuminate\Http\Request;
 use Redis;
 class ItemsController extends Controller
 {
-    public function GetItems(Request $request){
-        $page = $request->input('page');
-        try {
-            $data = Redis::get('data');
-        } catch (Exception $e) {
-          return "error";
-        }
-        $data = json_decode($data);
-        $resp = [];
-        for ($i=0; $i < 3; $i++) {
-          $resp = $data->data[12 + $page*3+$i];
-        }
-        return json_encode($resp);
-    }
+
 }
