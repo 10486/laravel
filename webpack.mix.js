cada5bf1bc632ae7d16b3017106368c13e9bbff9
10486 <tsyduev.b.1@gmail.com>
2019-12-01 14:32:56 +0500

    dfd

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/script.js', 'public/js/app.js')
    .js('resources/js/bootstrap.js', 'public/js/app.js')
+   .js('resources/js/scroller.js', 'public/js/app.js')   
    .js('resources/js/basket-handler.js', 'public/js/app.js')
    .sass('resources/sass/_variables.scss', 'public/css/app.css')
    .styles(['resources/sass/style.css','resources/sass/icomoon.css'], 'public/css/app.css')
