5c36e44379d007e23f61408de58849d6d9e88507
10486 <tsyduev.b.1@gmail.com>
2019-12-01 12:45:38 +0500

    sdf

--- resources/views/her.blade.php
+++ resources/views/her.blade.php
@@ -1,15 +1,18 @@
 @section('her')
 <div id="btn" style=" background-color: rgb(55, 0, 255); margin: 300px auto; font-size: 100px;">ЖМИ</div>
 <script>
-    $('#btn').click(() => {
-        $.ajax({
-            type: 'GET',
-            url: '/get_items',
-            data: '255&666&228&322&1488&420&69',
-            success:function (data) {
-              console.log(data);
-            }
-        })
-    });
+window.onload = function () {
+  $('#btn').click(() => {
+      $.ajax({
+          type: 'GET',
+          url: '/get_items',
+          data: '255&666&228&322&1488&420&69',
+          success:function (data) {
+            console.log(data);
+          }
+      })
+  });
+}
+
 </script>
 @show
