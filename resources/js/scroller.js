4eb7f9fa64b1a850f1b472286a92401922595c46
10486 <tsyduev.b.1@gmail.com>
2019-12-01 14:59:05 +0500

    asd

--- resources/js/scroller.js
+++ resources/js/scroller.js
@@ -12,4 +12,5 @@ $(window).scroll(function () {
  	for (var i = 0; i < data.lenght; i++) {
  		$(".catalog-grid").html() + `<div class="catalog-item" id="${data[i]['id']}"><img src="${data[i]['ImageSrc']}" alt="Картинка товара" class="catalog-item__img"><p class="catalog-item__name">${data[i]['title']}</p><p class="catalog-item__price-new">${data[i]['price-final']} P</p><p class="catalog-item__price-old">${data['price']} P</p><div class="button-wrapper"><button class="catalog-item__basket-button"><img src="img/basket.png" alt=""></button></div></div>`
  	}
+  sleep(1000);
 }});}});
